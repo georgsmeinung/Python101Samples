f987b13a32c9d249d8694d3797a007828782af99
georgsmeinung <jenicolau@gmail.com>
2020-10-06 10:24:55 -0300

    New Problem

---
+++ hellowolrdframe.py
@@ -0,0 +1,40 @@
+"""
+Hello World in a frame
+----------------------
+Write a function that takes a list of strings and 
+prints them, one per line, in a rectangular frame. 
+For example the list 
+["Hello", "World", "in", "a", "frame"] 
+gets printed as:
+*********
+* Hello *
+* World *
+* in    *
+* a     *
+* frame *
+*********
+"""
+
+def getLargestElement(scanlist):
+    maxlen=0
+    for i in scanlist:
+        if len(i)>maxlen: 
+            maxlen=len(i)
+    return maxlen
+
+def drawFrame(message):
+    maxwidth=getLargestElement(message)+4
+    print("*"*maxwidth)
+    for i in range(len(message)):
+        print("* {:^{width}} *".format(message[i],width=(maxwidth-4)))
+    print("*"*maxwidth)
+
+def toList(stringMsg):
+    outList=list(stringMsg.split(" "))
+    return outList
+
+print("Hello World in a frame")
+print("----------------------")
+text=input("Enter a message: ")
+text=toList(text)
+drawFrame(text)
